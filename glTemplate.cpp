7633227e78b1c56fc9e6254277de9c04468601f3
lazypupz++ <antonycasey08@gmail.com>
2024-12-02 11:03:24 +0000

    Update glTemplate.cpp

--- glTemplate.cpp
+++ glTemplate.cpp
@@ -3,25 +3,47 @@
 #include <iostream>
 
 
-void framebuffer_size_callback(GLFWWindow* window, int width, int height){
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0,0,800,600);
 }
 
-void processInput(GLFWWindow* window){
+void processInput(GLFWwindow* window){
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
       glfwShouldCloseWindow(window, true);
   
     }
 }
 
+const char* vertexShaderSrc = "R(
+    #version 330 core
+    layout( location = 0) in vec3 aPos;
+    void main(){
+        gl_Postition = vec4(aPos, 1.0f);
+    }
+)";
+
+const char* fragmentShaderSrc = "R(
+    #version 330 core 
+    out vec4 fragColour;
+    void main(){
+        fragColour = vec4(0.8, 0.5, 0,2, 1,0);
+    }
+
+)";    
+
 int main(){
-    glfwInit();
+    if(!glfwInit()){
+        std::cerr << "Failed to initialise GLFW" << std::endl;
+        glfwTerminate();
+        return -1;
+        
+    }
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_COMPAT_PROFILE);
 
     
-    GLFWWindow* window = glfwCreateWindow(800,600,"OpenGL Window", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800,600,"OpenGL Window", nullptr, nullptr);
     if(window == NULL){
       std::cerr << "Failed To Open Window " << std::endl;
       return -1;
@@ -33,9 +55,17 @@ int main(){
       return -1;
     }  
 
+    GLuint VBO, VAO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER,)
+    
+    glGenVertexArray(1, &VAO);
+
+    unsigned int vertexShader = glfwCreateShader(GL_VERTEX_SHADER);
+    
     while(!glfwWindowShouldClose(window){
 
-      processInput();
+      processInput(window);
       
       glfwSwapBuffers(window);
       glfwPollEvents();
